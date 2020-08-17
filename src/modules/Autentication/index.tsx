91de5cfe3b2c380ed274b0d738681c3d898bc3d2
Camilo Victoria <camiloavict@gmail.com>
2020-08-17 17:03:40 -0500

    SKM-04-view finished

--- src/modules/Autentication/index.tsx
+++ src/modules/Autentication/index.tsx
@@ -5,17 +5,26 @@ import { FormSection } from '../../pages/Login/Login.style';
 import conEdison_logo from '../../assets/icons/conEdison_logo.png';
 
 
-const Autentication = (props: any) => {
-    
-    const inputRef = React.useRef<HTMLInputElement>(null);
+const Autentication = () => {
+    const [user, setUser] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
 
+    const inputRef = useRef<HTMLInputElement>(null);
+    
+    const handleDisable = ():boolean => {
+        if(user === '' || password === '') return true
+        return false
+    }
+    
     return (
         <FormSection>
             <figure className='image__container'>
                 <img src={conEdison_logo} alt="conEdison logo" width="180" />
             </figure>
 
-            <Input
+            <Input 
+                value={user}
+                onChange={event => setUser(event.currentTarget.value)}
                 inputRef={inputRef}
                 type="text"
                 placeholder="User"
@@ -30,6 +39,8 @@ const Autentication = (props: any) => {
                 }}
             />
             <Input
+                value={password}
+                onChange={event => setPassword(event.currentTarget.value)}
                 inputRef={inputRef}
                 placeholder="Password"
                 type="password"
@@ -44,11 +55,13 @@ const Autentication = (props: any) => {
                 }}
             />
 
-            <Button type = "submit" disabled 
+            <Button type = "submit" disabled = {handleDisable()}
                 // onClick={() => inputRef.current && inputRef.current.focus()}
                 overrides={{ BaseButton: { style: { width: "80%" } } }}>LOGIN
             </Button>
         </FormSection>
+
+        
     )
 }
 
