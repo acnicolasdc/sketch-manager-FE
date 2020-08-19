411ce9a19c98357bd2f90390a551af9ceba54ed2
Camilo Victoria <camiloavict@gmail.com>
2020-08-18 20:33:31 -0500

    SKM-04-finished

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 // import Autentication from "modules/Layout/components";
 import { GeneralSection } from './Login.style';
 import { LoginSection } from './Login.style';
-import Form from '../../modules/Form/Components/index';
+import Form from '../../modules/Form/components/index';
 import Layout from 'modules/Layout/components';
 
 const Login = () => {
